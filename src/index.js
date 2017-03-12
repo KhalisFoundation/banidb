46ddd14492778e88d553d307da061b588af52180
Divjot Singh <bogas04@gmail.com>
2017-03-12 23:46:22 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+export const API_URL = `https://devapi.khajana.org/`;
+
+export const TYPES = [
+  'First Letter Start (Gurmukhi)',
+  'First Letter Anywhere (Gurmukhi)',
+  'Full Word (Gurakhar)',
+  'Full Word (English)',
+  'Romanized (English)',
+];
+
+export const SOURCES = {
+  all: 'All Sources',
+  G: 'Guru Granth Sahib Ji',
+  D: 'Dasam Granth Sahib',
+  B: 'Bhai Gurdas Ji Vaaran',
+  N: 'Bhai Nand Lal Ji Vaaran',
+  A: 'Amrit Keertan',
+  S: 'Bhai Gurdas Singh Ji Vaaran',
+};
+
+export const buildApiUrl = options => {
+
+  const {
+    q = false,              // String: Query string.
+    source = false,         // String: one of Object.keys(SOURCES).
+    type = false,           // Number: index of TYPES.
+    writer = false,         // Number: Writer ID - Check README.md.
+    raag = false,           // Number: Raag ID - Check README.md.
+    ang = false,            // Number: Page number of the source.
+    results = false,        // Number: Count of results you want.
+    offset = false,         // Number: Used for pagination.
+    id = false,             // Number: Shabad ID.
+    hukam = false,          // Boolean: Pass true if you want hukamnama of today.
+    akhar = false,          // Boolean: Pass true to convert query string (gurulipi) into unicode text.
+    lipi = false,           // Boolean: Pass true to convert query string (unicode) into gurulipi text.
+    random = false          // Boolean: Pass true to get random shabad.
+  } = options;
+
+  let url = API_URL;
+
+  if (q !== false) {
+
+    let params = [];
+
+    if (source) params.push(`source=${source}`);
+
+    if (type) params.push(`searchtype=${type}`);
+
+    if (writer) params.push(`writer=${writer}`);
+
+    if (raag) params.push(`raag=${raag}`);
+
+    if (ang) params.push(`ang=${ang}`);
+
+    if (results) params.push(`results=${results}`);
+
+    if (offset) params.push(`offset=${offset}`);
+
+    url += `search/${q}?${params.join('&')}`;
+
+  } else if (id !== false) {
+
+    url += `shabad/${id}`;
+
+  } else if (ang !== false) {
+
+    url += `ang/${ang}/${source ? source : ''}`;
+
+  } else if (hukam !== false) {
+
+    url += `hukamnama/today`;
+
+  } else if (akhar !== false && lipi !== false) {
+
+    url += `akhar/${lipi}`;
+
+  } else if (random !== false) {
+
+    url += `random`;
+
+  } else {
+
+    throw new Error('Invalid options sent');
+
+  }
+
+  return url;
+}
