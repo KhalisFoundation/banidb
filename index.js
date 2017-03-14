18c4a41cc1d8a8a58ca8f167a9dc54679975d171
Divjot Singh <bogas04@gmail.com>
2017-03-14 05:43:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,43 +5,42 @@ const $viewer = document.querySelector('.shabad-viewer');
 fetch(url)
   .then(r => r.json())
   .then(r => {
-    if (r.error) {
-
-      $viewer.dataset.error = `Sorry, we are facing troubles in getting your shabad`;
-
-      $viewer.dataset.loading = "false";
-
-    } else {
-
-      $viewer.dataset.error = "";
-
-      $viewer.dataset.loading = "true";
-
-      const {
-        shabadinfo: { pageno: ang, source: { english: granth }, writer: { english: author } },
-        gurbani: lines
-      } = r;
-
-      $viewer.querySelector('.author').innerText = author;
-
-      $viewer.querySelector('.ang').innerText = ang;
-
-      $viewer.querySelector('.granth').innerText = granth;
-
-      $viewer.querySelector('.bani').innerHTML = lines
-        .map(({ shabad }) => `
-                <div class='shabad-line'>
-                  <p>${shabad.gurbani.unicode}</p>
-                  <blockquote>${shabad.translation.english.ssk}</blockquote>
-                </div>
-              `)
-        .join('');
-
-      $viewer.dataset.loading = "false";
+    if (r.error) {   
+      return Promise.reject(r);
     }
+
+    $viewer.dataset.error = "";
+
+    $viewer.dataset.loading = "true";
+
+    const {
+      shabadinfo: { pageno: ang, source: { english: granth }, writer: { english: author } },
+      gurbani: lines
+    } = r;
+
+    $viewer.querySelector('.author').innerText = author;
+
+    $viewer.querySelector('.ang').innerText = ang;
+
+    $viewer.querySelector('.granth').innerText = granth;
+
+    $viewer.querySelector('.bani').innerHTML = lines
+  
+      .map(({ shabad }) => `
+        <div class='shabad-line'>
+          <p>${shabad.gurbani.unicode}</p>
+          <blockquote>${shabad.translation.english.ssk}</blockquote>
+        </div>
+      `)
+  
+      .join('');
+
+    $viewer.dataset.loading = "false";
   })
   .catch(err => {
     console.error(err);
+  
     $viewer.dataset.loading = "false";
+  
     $viewer.dataset.error = `Sorry, we are facing troubles in getting your shabad`;
   });
