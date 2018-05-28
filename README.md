16113f4c3b6f991604ad8cd9a7b167a722914f0b
Divjot Singh <bogas04@gmail.com>
2018-05-28 13:47:12 +0530

    Rename to banidb, change source.R

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# khajana-js 💎
+# banidb 💎
 
-A JavaScript wrapper for [Khajana](http://khajana.org/) REST [API](https://github.com/KhalisFoundation/KhajanaAPI)
+A JavaScript wrapper for [Khajana](http://banidb.com/) REST [API](https://github.com/KhalisFoundation/BaniDB-API)
 
 # Usage
 
@@ -9,6 +9,6 @@ So you can use it directly in a script tag, or you can `require` it.
 
 Refer to following demos to use it.
 
-## [node](https://github.com/bogas04/khajana-js/blob/node/index.js#L1)
+## [node](https://github.com/bogas04/banidb/blob/node/index.js#L1)
 
-## [web](https://github.com/bogas04/khajana-js/blob/gh-pages/index.js#L1)
+## [web](https://github.com/bogas04/banidb/blob/gh-pages/index.js#L1)
--- src/index.js
+++ src/index.js
@@ -1,46 +1,44 @@
 export const TYPES = [
-  'First letter each word from start (Gurmukhi)',
-  'First letter each word anywhere (Gurmukhi)',
-  'Full Word (Gurmukhi)',
-  'Full Word Translation (English)',
-  'Romanized Gurmukhi (English)',
+  "First letter each word from start (Gurmukhi)",
+  "First letter each word anywhere (Gurmukhi)",
+  "Full Word (Gurmukhi)",
+  "Full Word Translation (English)",
+  "Romanized Gurmukhi (English)"
 ];
 
 export const SOURCES = {
-  all: 'All Sources',
-  G: 'Guru Granth Sahib Ji',
-  D: 'Dasam Granth Sahib',
-  B: 'Bhai Gurdas Ji Vaaran',
-  A: 'Amrit Keertan',
-  S: 'Bhai Gurdas Singh Ji Vaaran',
-  N: 'Bhai Nand Lal Ji Vaaran',
-  R: 'Rehatnamas and Tankhanamas'
+  all: "All Sources",
+  G: "Guru Granth Sahib Ji",
+  D: "Dasam Granth Sahib",
+  B: "Bhai Gurdas Ji Vaaran",
+  A: "Amrit Keertan",
+  S: "Bhai Gurdas Singh Ji Vaaran",
+  N: "Bhai Nand Lal Ji Vaaran",
+  R: "Rehatnamas & Panthic Sources"
 };
 
 export const buildApiUrl = options => {
-
   const {
-    q = false,              // String: Query string.
-    source = false,         // String: one of Object.keys(SOURCES).
-    type = false,           // Number: index of TYPES.
-    writer = false,         // Number: Writer ID - Check README.md.
-    raag = false,           // Number: Raag ID - Check README.md.
-    ang = false,            // Number: Page number of the source.
-    results = false,        // Number: Count of results you want.
-    offset = false,         // Number: Used for pagination.
-    id = false,             // Number: Shabad ID.
-    hukam = false,          // Boolean: Pass true if you want hukamnama of today.
-    akhar = false,          // Boolean: Pass true to convert query string (gurulipi) into unicode text.
-    lipi = false,           // Boolean: Pass true to convert query string (unicode) into gurulipi text.
-    random = false,         // Boolean: Pass true to get random shabad.
-    randomid = false,       // Boolean: Pass true to get random shabad id only.
-    API_URL = 'https://api.banidb.com/', // String: API_URL to hit. (Prod: api.banidb.com, Dev: devapi.khajana.org).
+    q = false, // String: Query string.
+    source = false, // String: one of Object.keys(SOURCES).
+    type = false, // Number: index of TYPES.
+    writer = false, // Number: Writer ID - Check README.md.
+    raag = false, // Number: Raag ID - Check README.md.
+    ang = false, // Number: Page number of the source.
+    results = false, // Number: Count of results you want.
+    offset = false, // Number: Used for pagination.
+    id = false, // Number: Shabad ID.
+    hukam = false, // Boolean: Pass true if you want hukamnama of today.
+    akhar = false, // Boolean: Pass true to convert query string (gurulipi) into unicode text.
+    lipi = false, // Boolean: Pass true to convert query string (unicode) into gurulipi text.
+    random = false, // Boolean: Pass true to get random shabad.
+    randomid = false, // Boolean: Pass true to get random shabad id only.
+    API_URL = "https://api.banidb.com/" // String: API_URL to hit. (Prod: api.banidb.com, Dev: devapi.khajana.org).
   } = options;
 
   let url = API_URL;
 
   if (q !== false) {
-
     let params = [];
 
     if (source) params.push(`source=${source}`);
@@ -57,37 +55,22 @@ export const buildApiUrl = options => {
 
     if (offset) params.push(`offset=${offset}`);
 
-    url += `search/${q}?${params.join('&')}`;
-
+    url += `search/${q}?${params.join("&")}`;
   } else if (id !== false) {
-
     url += `shabad/${id}`;
-
   } else if (ang !== false) {
-
-    url += `ang/${ang}/${source ? source : ''}`;
-
+    url += `ang/${ang}/${source ? source : ""}`;
   } else if (hukam !== false) {
-
     url += `hukamnama/today`;
-
   } else if (akhar !== false && lipi !== false) {
-
     url += `akhar/${lipi}`;
-
   } else if (randomid !== false) {
-
     url += `random/id`;
-
   } else if (random !== false) {
-
     url += `random`;
-
   } else {
-
-    throw new Error('Invalid options sent');
-
+    throw new Error("Invalid options sent");
   }
 
   return url;
-}
+};
